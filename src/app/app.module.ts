305d91797c359639ff2d55c5bf89cdcec00ab2b8
Paulo Elienay <paulo@pauloelienay.com>
2021-07-03 18:19:15 -0300

    chore(Module): Adds `FormsModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
